0dbea5acdfdf45ceec461ed9b4365141b2eae1f7
apple-yagi <applesryu@gmail.com>
2025-04-19 23:10:53 +0900

    Update Graphviz import to use wasm-graphviz package

--- packages/core/src/write/svg.ts
+++ packages/core/src/write/svg.ts
@@ -1,4 +1,4 @@
-import { Graphviz } from "@hpcc-js/wasm";
+import { Graphviz } from "@hpcc-js/wasm-graphviz";
 import { generateDot, type Edge } from "../dot/index.ts";
 import { writeFileSync, type PathLike } from "node:fs";
 
@@ -6,6 +6,6 @@ export async function writeSvgSync(edges: Edge[], file: PathLike) {
 	const dot = generateDot(edges);
 
 	const graphviz = await Graphviz.load();
-	const svg = await graphviz.layout(dot, "svg");
+	const svg = graphviz.layout(dot, "svg");
 	writeFileSync(file, svg, "utf-8");
 }
